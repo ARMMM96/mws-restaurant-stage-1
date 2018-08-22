53573562d5cb6eab886692a3feac5a623d00f5e6
Ahmed <ahmed.rabea.mahmoud@gmail.com>
2018-08-22 22:34:04 +0200

    Fixing Offline Availability Issue

--- sw.js
+++ sw.js
@@ -1,8 +1,38 @@
 const cacheName = 'mws';
 
+const cacheAssets = [
+  '/',
+  '/index.html',
+  '/restaurant.html',
+  '/css/styles.css',
+  '/js/main.js',
+  '/js/restaurant_info.js',
+  '/data/restaurants.json',
+  '/img/1.jpg',
+  '/img/2.jpg',
+  '/img/3.jpg',
+  '/img/4.jpg',
+  '/img/5.jpg',
+  '/img/6.jpg',
+  '/img/7.jpg',
+  '/img/8.jpg',
+  '/img/9.jpg',
+  '/img/10.jpg'
+];
+
 // Call Install Event
 self.addEventListener('install', e => {
   console.log('Service Worker: Installed');
+
+  e.waitUntil(
+    caches
+    .open(cacheName)
+    .then(cache => {
+      console.log('Service Worker: Caching Files');
+      cache.addAll(cacheAssets);
+    })
+    .then(() => self.skipWaiting())
+  );
 });
 
 // Call Activate Event
@@ -26,18 +56,5 @@ self.addEventListener('activate', e => {
 // Call Fetch Event
 self.addEventListener('fetch', e => {
   console.log('Service Worker: Fetching');
-  e.respondWith(
-    fetch(e.request)
-      .then(res => {
-        // Make copy/clone of response
-        const resClone = res.clone();
-        // Open cahce
-        caches.open(cacheName).then(cache => {
-          // Add response to cache
-          cache.put(e.request, resClone);
-        });
-        return res;
-      })
-      .catch(err => caches.match(e.request).then(res => res))
-  );
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
